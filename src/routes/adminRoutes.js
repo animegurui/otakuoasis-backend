5ff568d64f350d135a2e89fd1b78d61c2948feb2
animegurui <favorsmith8422@gmail.com>
2025-08-23 21:44:18 +0100

    Delete src/routes/adminRoutes.js

--- src/routes/adminRoutes.js
+++
@@ -1,6 +0,0 @@
-router.get('/dashboard', (req, res) => {
-  res.json({
-    animeCount: await Anime.countDocuments(),
-    popularAnime: await Anime.find().sort({ popularity: -1 }).limit(5)
-  });
-});
